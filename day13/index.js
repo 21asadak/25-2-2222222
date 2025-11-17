d5352ca75def915cc4d932e6fdfe99f77503534b
25-2-2222222 <21asadak@naver.com>
2025-11-17 10:25:08 +0900

    nnnn

---
+++ day13/index.js
@@ -0,0 +1,37 @@
+export default async function main() {
+
+    console.log('start App')
+
+
+
+   let currentIndex = 0;
+   const menuitems = document.querySelectorAll(".menu-item");
+
+
+   console.log(menuitems)
+
+   menuitems[currentIndex].classList.add("select")
+
+   window.addEventListener("keydown", (e) => {
+
+
+    menuitems[currentIndex].classList.remove('select')
+
+    console.log(e.key)
+
+    if(e.key == "ArrowUp")  {
+        currentIndex--
+        if(currentIndex < 0) {
+            currentIndex = 2
+        }
+    }
+    else if(e.key == "ArrowDown") {
+        currentIndex++
+        currentIndex %=3
+    }
+    console.log(currentIndex)
+    menuitems[currentIndex].classList.add('select')
+
+     })
+
+}
